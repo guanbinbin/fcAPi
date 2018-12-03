63612cdf5906843ff13f241759da4e07a157c5b7
liqiang <906653099@qq.com>
2018-12-03 21:12:31 +0800

    login and user list

---
+++ api/user/controller/User.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\user\controller;
+
+use think\Db;
+
+// http://localhost/tp5/index.php/user/User/addUser
+
+ // header('Access-Control-Allow-Origin: *');
+class User
+{   
+    public function addUser()
+    {   
+    	$data=Db::table('userInfo')->select();
+        return json($data);
+    }
+    
+    
+}
