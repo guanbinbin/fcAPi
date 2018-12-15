93d5f61f0717e4b1262af105455c89c31bc89829
liqiang <906653099@qq.com>
2018-12-15 16:17:59 +0800

    黑名单接口

--- api/client/controller/Client.php
+++ api/client/controller/Client.php
@@ -29,6 +29,27 @@ header('Access-Control-Allow-Headers: content-type');
     	}
         return json($res);
  	}
+
+ 	/*
+ 	*  查询客户黑名单
+ 	*/ 
+ 	public function getBlackList(){
+ 		$data=Db::name('black_user')->select();
+    	if(isset($data) && count($data)){
+    		$res=Array('code'=>200,
+    			'success' => true,
+    			'msg'=>'获取到用户列表',
+    			'data'=>$data
+    		);
+    	}else {
+    		$res=Array('code'=>-1,
+    			'success' => true,
+    			'msg'=>'用户为空',
+    			'data'=>0
+    		);
+    	}
+        return json($res);
+ 	}
  }
 
 
