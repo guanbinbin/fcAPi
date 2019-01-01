9751cce5b144a7bed600e7aeb21233f298b6fb3b
liqiang <906653099@qq.com>
2019-01-01 13:55:20 +0800

    更新，删除用户，搜索功能

--- api/user/controller/User.php
+++ api/user/controller/User.php
@@ -16,12 +16,13 @@ header('Access-Control-Allow-Methods: GET, POST, PUT');
 class User extends Controller{   
     public function getUserList()
     {   
-    	$data=Db::name('user')->select();
+    	$data=Db::name('user')->limit(1,10)->select();
+        $total=Db::name('user')->select();
     	if(isset($data) && count($data)){
     		$res=Array('code'=>200,
     			'success' => true,
     			'msg'=>'获取到用户列表',
-                'number'=> count($data),
+                'number'=> count($total),
     			'data'=>$data
     		);
     	}else {
@@ -36,7 +37,7 @@ class User extends Controller{
     }
 
     /*
-    *  查询用户
+    * 查询用户
     * params
     * id: array
     * type: default is null or 0
@@ -45,12 +46,6 @@ class User extends Controller{
     * pageSize: default is 20 
     */ 
     public function queryUser(){
-
-        // $a=array("red","green");
-        // array_push($a,"blue","yellow");
-        // print_r($a);
-
-
         $request = Request::instance();
         $id = $request->param('id');
         $type =  $request->param('type');
@@ -59,6 +54,7 @@ class User extends Controller{
         $pageSize =  $request->param('pageSize');
 
         $sql = array();
+        $limit = array();
         if(isset($id) && !empty($id)) {
            array_push($sql,array('id'=>$id));
         };
@@ -70,22 +66,33 @@ class User extends Controller{
            array_push($sql,array('keyword'=>$keyword));
         };
 
+
+
         if(isset($pageIndex) && !empty($pageIndex)) {
-           array_push($sql,array('pageIndex'=>$pageIndex));
+           array_push($limit,array('pageIndex'=>$pageIndex));
         }else {
-          array_push($sql,array('pageIndex'=>1));
+          array_push($limit,array('pageIndex'=>1));
         };
 
         if(isset($pageSize) && !empty($pageSize)) {
-           array_push($sql,array('pageSize'=>$pageSize));
+           array_push($limit,array('pageSize'=>$pageSize));
         }else {
-            array_push($sql,array('pageSize'=>10));
+            array_push($limit,array('pageSize'=>10));
         };
 
-        print_r($sql);
-
-
-
+        
+        if(count($sql)) {
+            $condition = array();
+            foreach($sql as $key => $value) {
+              foreach($sql[$key] as $k => $val){
+                //array_push($condition, $k)
+              }
+              
+            }
+            print_r($condition);
+        }else {
+            
+        }
 
     }
 
@@ -140,6 +147,19 @@ class User extends Controller{
     }
 
     public function updateUser(){
+        $request = Request::instance();
+        $obj =  $request->param('obj');
+
+        $arr['id'] = $obj['id'];
+        $arr['name'] = $obj['name'];
+        $arr['type'] = $obj['type'];
+        $arr['pwd'] = $obj['pwd'];
+        $arr['phone'] = $obj['phone'];
+        $arr['qq'] = $obj['qq'];
+
+
+        $res = Db::name('user')->update($arr);
+        return json($res);
     }
 
     public function filterUser(){
@@ -163,7 +183,7 @@ class User extends Controller{
     			'data'=>$data
     		);
     	}else {
-    		$res=Array('code'=>-1,
+    		$res=Array('code'=>0,
     			'success' => true,
     			'msg'=>'用户为空',
     			'data'=>0
@@ -180,9 +200,8 @@ class User extends Controller{
       $request = Request::instance();
       $type =  $request->param('type');
       $condition =  $request->param('condition');
-      $query = "1";
 
-      $data = Db::name('user')->where('id|name|qq|phone|date','like',1)->select();
+      $data = Db::name('user')->where('type',$type)->where('id|name','like',"%{$condition}")->select();
 
       if(isset($data) && count($data)){
     		$res=Array('code'=>200,
@@ -200,4 +219,43 @@ class User extends Controller{
 
       return json($data);
     } 
+
+    public function pageUserList(){
+      /*
+      * pageIndex： 当前页码
+      * pageSize： 个数
+      */ 
+      $request = Request::instance();
+      $pageIndex =  $request->param('pageIndex');
+      $pageSize =  $request->param('pageSize');
+
+      $data=Db::name('user')->page($pageIndex,$pageSize)->select();
+      if(isset($data)){
+        $res=Array('code'=>200,'success' => true,'msg'=>'成功获取用户列表','data'=>$data);
+      }else {
+        $res=Array('code'=>-1,
+                'success' => true,
+                'msg'=>'用户为空',
+                'data'=>0
+            );
+      }
+      
+      return json($res);
+    }
+
+    public function deleteUser(){
+        $request = Request::instance();
+        $id = $request->param('id');
+
+        $res = Db::name('user')->delete($id);
+
+
+        if(!empty($res)) {
+            $data = Array("code"=>200,"msg"=>"删除成功！","data"=>$res);
+        }else {
+            $data = Array("code"=>0,"msg"=>"删除失败！","data"=>0);
+        }
+        return json($data);
+
+    }
 }
